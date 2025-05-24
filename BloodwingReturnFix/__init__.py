e3aa8b99b63e228be7a5939ec1ba83f21cf3ed5b
RedxYeti <39033204+RedxYeti@users.noreply.github.com>
2025-05-24 02:29:33 -0400

    Add files via upload

--- BloodwingReturnFix/__init__.py
+++ BloodwingReturnFix/__init__.py
@@ -3,7 +3,7 @@ from mods_base import SETTINGS_DIR
 from mods_base import build_mod
 from unrealsdk import logging
 
-from .hooks import BloodwingCalled
+from .hooks import BloodwingCalled, BloodwingDetonated
 
 # Gets populated from `build_mod` below
 __version__: str
@@ -15,10 +15,56 @@ build_mod(
     # version_info_parser=lambda v: tuple(int(x) for x in v.split(".")),
     # deregister_same_settings=True,      # This is True by default
     keybinds=[],
-    hooks=[BloodwingCalled],
+    hooks=[BloodwingCalled, BloodwingDetonated],
     commands=[],
     # Defaults to f"{SETTINGS_DIR}/dir_name.json" i.e., ./Settings/bl1_commander.json
     settings_file=Path(f"{SETTINGS_DIR}/BloodwingReturnFix.json"),
 )
 
 logging.info(f"Bloodwing Return Fix: {__version__}, {__version_info__}")
+import unrealsdk #type: ignore
+from unrealsdk import logging #type: ignore
+from unrealsdk.hooks import Type, add_hook, remove_hook, Block #type: ignore
+from unrealsdk.unreal import UObject, WrappedStruct, BoundFunction, UScriptStruct #type: ignore
+from mods_base import hook,get_pc,ENGINE #type: ignore
+import os
+
+Initialized = False
+file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "CommandHistory.txt")
+def ConsoleCommandHistory(obj: UObject, args: WrappedStruct, ret: any, func: BoundFunction):
+    global file_path
+    if not os.path.exists(file_path): 
+        with open(file_path, 'w') as file:
+            file.write("fart")  
+
+    if not os.access(file_path, os.W_OK):#readonly
+        return
+    
+    with open(file_path, 'w') as file:
+        file.write("")
+
+    for command in obj.History:
+        with open(file_path, 'a') as file:
+            file.write(command + '\n')
+
+    return
+
+remove_hook("Engine.Console:ConsoleCommand", Type.POST, "ConsoleCommandHistory")
+add_hook("Engine.Console:ConsoleCommand", Type.POST, "ConsoleCommandHistory", ConsoleCommandHistory)
+
+def FillHistory(obj: UObject, args: WrappedStruct, ret: any, func: BoundFunction):
+    global Initialized, file_path
+    if Initialized:
+        return
+    
+    Initialized = True
+
+
+    with open(file_path, 'r') as file:
+        obj.History = [line.strip() for line in file.readlines()]
+
+    logging.info(f'{obj.History}')
+    return
+
+remove_hook("Engine.Console:Initialized", Type.POST, "FillHistory")
+add_hook("Engine.Console:Initialized", Type.POST, "FillHistory", FillHistory)
--- BloodwingReturnFix/hooks.py
+++ BloodwingReturnFix/hooks.py
@@ -4,15 +4,25 @@ from unrealsdk.hooks import Type
 from unrealsdk.unreal import UObject, WrappedStruct, BoundFunction
 from mods_base import hook,get_pc
 
-@hook(
-    hook_func="WillowGame.WillowProjectile:RunCustomEvent",
-    hook_type=Type.POST,
-)
+@hook(hook_func="WillowGame.WillowProjectile:RunCustomEvent",hook_type=Type.POST,)
 def BloodwingCalled(
     obj: UObject,
     __args: WrappedStruct,
     __ret: any,
     __func: BoundFunction,
 ) -> None:
-    if __args.EventName == "Return" and obj.Class.Name == "WillowBloodwingProjectile":
+    if obj.Class.Name == "WillowBloodwingProjectile" and __args.EventName == "Return":
         obj.CollisionComponent = None
+
+@hook(
+    hook_func="WillowGame.WillowProjectile:Detonate",
+    hook_type=Type.POST,
+)
+def BloodwingDetonated(
+    obj: UObject,
+    __args: WrappedStruct,
+    __ret: any,
+    __func: BoundFunction,
+) -> None:
+    if obj.Class.Name == "WillowBloodwingProjectile":
+        obj.InstigatorController.RetrieveBloodwing(False, 0)
