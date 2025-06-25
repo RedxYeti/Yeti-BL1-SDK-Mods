eba179aa76d2840ea5ed25e80fb7cbc8a60bb172
RedxYeti <39033204+RedxYeti@users.noreply.github.com>
2025-06-25 19:53:54 -0400

    Add files via upload

--- NoAimPunch/__init__.py
+++ NoAimPunch/__init__.py
@@ -1,10 +1,50 @@
-from mods_base import hook, build_mod
+from mods_base import hook, build_mod, BoolOption
+from unrealsdk import find_class
 from unrealsdk.hooks import Type,Block
 
+globals_default = find_class('GearboxFramework.GearboxGlobals').ClassDefaultObject
 
-@hook("WillowGame.WillowPlayerController:ShakeView", Type.PRE)
-def ShakeView(obj, args, ret, func):
-    if args.NewViewShake.RotMag.Z == 400 and args.NewViewShake.RotRate.Z == 20 and args.NewViewShake.RotTime == 0.25:
+@hook("WillowGame.WillowPlayerPawn:PlayDamageCameraShake", Type.PRE)
+def PlayDamageCameraShake(*_):
+    return Block
+
+
+@hook("WillowGame.WillowPlayerController:DamageShake", Type.PRE)
+def DamageShake(obj, args, ret, func):
+    if not oidShake.value:
+        return
+
+    Globals = globals_default.GetGearboxGlobals()
+
+    if Globals:
+        GlobalsDef = Globals.GetGlobalsDefinition()
+
+    if not GlobalsDef:
+        print("yo")
         return Block
+    
+    BigDamageThreshold = GlobalsDef.BigDamageThreshold
+    BigDamageShake = GlobalsDef.BigDamageShake
+    SmallDamageShake = GlobalsDef.SmallDamageShake
+    
+    Damage = min(args.Damage, 100)
+
+    if(Damage > BigDamageThreshold):
+        DamageShake = BigDamageShake
+    else:
+        DamageShake = SmallDamageShake
+
+    if(DamageShake != None):
+        obj.ClientPlayForceFeedbackWaveform(DamageShake)
+    return Block
+
+
+oidShake = BoolOption(
+    "Remove Screen Shake",
+    False,
+    "On",
+    "Off",
+    description="This will disable the screen shake as well."
+)
 
 build_mod()
